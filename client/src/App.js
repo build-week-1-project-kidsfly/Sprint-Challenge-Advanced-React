aa55845c5994db40b7149df60206bda3be8f1098
josepaniagua <jpanjr@gmail.com>
2019-12-20 17:36:10 -0800

    player

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import PlayerList from "./components/PlayerList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//Styling
+import "./App.css";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      playerData: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/api/players")
+      .then(res => {
+        this.setState({ playerData: res.data });
+        console.log(res.data);
+      })
+      .catch(err => {
+        console.log("Error: ", err);
+      });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Women's World Cup</h1>
+        <PlayerList />
+      </div>
+    );
+  }
 }
 
 export default App;
